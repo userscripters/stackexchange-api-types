281ad10c5810d74ac06f2f5cd603b43a2c75ed09
Oleg Valter <o.a.philippov@gmail.com>
2021-10-29 05:50:22 +0300

    generated type definitions for the CommonWrapperObject

---
+++ lib/wrapper.d.ts
@@ -0,0 +1,15 @@
+export interface CommonWrapperObject<T extends object = object> {
+    backoff?: number;
+    error_id?: number;
+    error_message?: string;
+    error_name?: string;
+    has_more: boolean;
+    items: T[];
+    page?: number;
+    page_size?: number;
+    quota_max: number;
+    quota_remaining: number;
+    total?: number;
+    type?: string;
+}
+export as namespace StackExchangeAPI;
