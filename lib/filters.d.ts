fe9f58f423ba63306949597662e8a08ab9c31220
Oleg Valter <o.a.philippov@gmail.com>
2021-11-02 02:26:03 +0300

    regenerated filters type definitions file

--- lib/filters.d.ts
+++ lib/filters.d.ts
@@ -1,3 +1,11 @@
 export namespace Filters {
     export type BuiltIn = "default" | "withbody" | "default" | "none" | "total";
 }
+
+declare module global {
+    namespace StackExchangeAPI {
+        namespace Filters {
+            type BuiltIn = "default" | "withbody" | "default" | "none" | "total";
+        }
+    }
+}
