336ddb38364b84d407f45cec749a4fe0f80b2f2b
Oleg Valter <o.a.philippov@gmail.com>
2021-11-02 04:15:26 +0300

    moved types generation to own module

--- generator/index.ts
+++ generator/index.ts
@@ -1,11 +1,6 @@
 import got from "got";
-import type ts from "typescript";
-import { __String } from "typescript";
-import { URL } from "url";
 import { generateBuiltInFilters } from "./filters.js";
-import { InterfaceOptions, parseInterface } from "./parsers.js";
-import { printNodesToFile } from "./printer.js";
-import { getDocument, partition, sleep } from "./utils.js";
+import { generateTypes } from "./types.js";
 import { generateResponseWrapper } from "./wrapper.js";
 
 const DOCS_BASE = "https://api.stackexchange.com";
@@ -20,125 +15,36 @@ got.extend({
     },
 });
 
-const docsHome = new URL(`${DOCS_BASE}/docs`);
-docsHome.searchParams.append("tab", "type");
-docsHome.hash = "docs";
+const typeReqSel = ".method-name > .excluded";
+const typeNameSel = ".mainbar > .subheader h1";
+const typeFieldsSel = ".indented > .method";
 
-const res = await got(docsHome);
+const { default: ts } = await import("typescript");
+const { factory } = ts;
 
-if (res.statusCode === 200) {
-    const { body } = res;
+const nsName = "StackExchangeAPI";
 
-    const { JSDOM } = await import("jsdom");
+const unionRegex =
+    /(?:(?:an )?array of )?(?:one of )?'?(\w+)'?(?:,? (?:or )?(?:but new options (?:can|may|might) be added\.)?|$)/i;
 
-    const {
-        window: { document },
-    } = new JSDOM(body);
+await generateResponseWrapper(
+    factory,
+    DOCS_BASE,
+    "/docs/wrapper",
+    `${TYPES_PATH}/wrapper.d.ts`,
+    typeNameSel,
+    typeFieldsSel,
+    typeReqSel,
+    unionRegex,
+    "Wrappers"
+);
 
-    const indented = [...document.querySelectorAll(".indented")];
+await generateBuiltInFilters(
+    factory,
+    DOCS_BASE,
+    "/docs/filters",
+    `${TYPES_PATH}/filters.d.ts`,
+    "Filters"
+);
 
-    const typeWrapSel = ".method";
-    const typeLinkSel = "p > a[href*='/docs/types']";
-    const typeReqSel = ".method-name > .excluded";
-    const typeNameSel = ".mainbar > .subheader h1";
-    const typeFieldsSel = ".indented > .method";
-
-    const typeWrappers = indented.filter(
-        (e) => !!e.querySelectorAll(typeWrapSel).length
-    );
-
-    const typePaths = typeWrappers
-        .map((e) => e.querySelector<HTMLAnchorElement>(typeLinkSel)?.href || "")
-        .filter(Boolean);
-
-    // some types are not linked to from the main page
-    typePaths.push(
-        ...[
-            "shallow-user",
-            "collective-external-link",
-            "collective-recommendation",
-            "badge-count",
-            "network-post",
-            "closed-details",
-            "original-question",
-            "migration-info",
-            "notice",
-            "related-site",
-            "styling",
-            "collective-membership",
-        ].map((p) => `/docs/types/${p}`)
-    );
-
-    const partitions = partition(typePaths, 3);
-
-    const { default: ts } = await import("typescript");
-    const { factory } = ts;
-
-    const nsName = "StackExchangeAPI";
-
-    const nodes: Map<__String, ts.Node> = new Map();
-
-    const unionRegex =
-        /(?:(?:an )?array of )?(?:one of )?'?(\w+)'?(?:,? (?:or )?(?:but new options (?:can|may|might) be added\.)?|$)/i;
-
-    const interfaceOptions: InterfaceOptions = {
-        exported: true,
-        overrides: {
-            event_id: factory.createKeywordTypeNode(
-                ts.SyntaxKind.NumberKeyword
-            ),
-        },
-    };
-
-    for (const partition of partitions) {
-        for (const path of partition) {
-            const document = await getDocument(DOCS_BASE, path);
-            if (!document) continue;
-
-            const iface = parseInterface(
-                factory,
-                document,
-                typeNameSel,
-                typeFieldsSel,
-                typeReqSel,
-                unionRegex,
-                interfaceOptions
-            );
-
-            const { escapedText } = iface.name;
-
-            nodes.has(escapedText) || nodes.set(escapedText, iface);
-        }
-
-        await sleep(1);
-    }
-
-    const unique: ts.Node[] = [];
-    for (const [, node] of nodes) {
-        unique.push(node, factory.createIdentifier("\n"));
-    }
-
-    unique.push(factory.createNamespaceExportDeclaration(nsName));
-
-    await printNodesToFile(ts, unique, `${TYPES_PATH}/types.d.ts`);
-
-    await generateResponseWrapper(
-        factory,
-        DOCS_BASE,
-        "/docs/wrapper",
-        `${TYPES_PATH}/wrapper.d.ts`,
-        typeNameSel,
-        typeFieldsSel,
-        typeReqSel,
-        unionRegex,
-        "Wrappers"
-    );
-
-    await generateBuiltInFilters(
-        factory,
-        DOCS_BASE,
-        "/docs/filters",
-        `${TYPES_PATH}/filters.d.ts`,
-        "Filters"
-    );
-}
+await generateTypes(factory, DOCS_BASE, `${TYPES_PATH}/types.d.ts`, nsName);
---
+++ generator/types.ts
@@ -0,0 +1,116 @@
+import type { NodeFactory, __String } from "typescript";
+import ts from "typescript";
+import { createModuleDeclaration, createNamespace } from "./factories.js";
+import { InterfaceOptions, parseInterface } from "./parsers.js";
+import { printNodesToFile } from "./printer.js";
+import { getDocument, interleave, partition, sleep } from "./utils.js";
+
+const addGlobalModifyingVersion = (
+    factory: NodeFactory,
+    namespaceName: string,
+    statements: ts.DeclarationStatement[]
+) => {
+    const ns = createNamespace(factory, namespaceName, statements);
+    return createModuleDeclaration(factory, "global", [ns], {
+        isAmbient: true,
+        isGlobal: true,
+    });
+};
+
+export const generateTypes = async (
+    factory: NodeFactory,
+    base: string,
+    filePath: string,
+    namespaceName: string
+) => {
+    const document = await getDocument(base, "/docs", {
+        hash: "docs",
+        parameters: { tab: "type" },
+    });
+    if (!document) return;
+
+    const indented = [...document.querySelectorAll(".indented")];
+
+    const typeWrapSel = ".method";
+    const typeLinkSel = "p > a[href*='/docs/types']";
+    const typeReqSel = ".method-name > .excluded";
+    const typeNameSel = ".mainbar > .subheader h1";
+    const typeFieldsSel = ".indented > .method";
+
+    const typeWrappers = indented.filter(
+        (e) => !!e.querySelectorAll(typeWrapSel).length
+    );
+
+    const typePaths = typeWrappers
+        .map((e) => e.querySelector<HTMLAnchorElement>(typeLinkSel)?.href || "")
+        .filter(Boolean);
+
+    // some types are not linked to from the main page
+    typePaths.push(
+        ...[
+            "shallow-user",
+            "collective-external-link",
+            "collective-recommendation",
+            "badge-count",
+            "network-post",
+            "closed-details",
+            "original-question",
+            "migration-info",
+            "notice",
+            "related-site",
+            "styling",
+            "collective-membership",
+        ].map((p) => `/docs/types/${p}`)
+    );
+
+    const partitions = partition(typePaths, 3);
+
+    const nodeMap: Map<__String, ts.DeclarationStatement> = new Map();
+
+    const unionRegex =
+        /(?:(?:an )?array of )?(?:one of )?'?(\w+)'?(?:,? (?:or )?(?:but new options (?:can|may|might) be added\.)?|$)/i;
+
+    const interfaceOptions: InterfaceOptions = {
+        exported: true,
+        overrides: {
+            event_id: factory.createKeywordTypeNode(
+                ts.SyntaxKind.NumberKeyword
+            ),
+        },
+    };
+
+    for (const partition of partitions) {
+        for (const path of partition) {
+            const document = await getDocument(base, path);
+            if (!document) continue;
+
+            const iface = parseInterface(
+                factory,
+                document,
+                typeNameSel,
+                typeFieldsSel,
+                typeReqSel,
+                unionRegex,
+                interfaceOptions
+            );
+
+            const { escapedText } = iface.name;
+
+            nodeMap.has(escapedText) || nodeMap.set(escapedText, iface);
+        }
+
+        await sleep(1);
+    }
+
+    const nodes = [...nodeMap.values()];
+
+    const pretty = interleave(nodes, factory.createIdentifier("\n"));
+
+    const global = addGlobalModifyingVersion(factory, namespaceName, nodes);
+
+    await printNodesToFile(
+        ts,
+        [...pretty, factory.createIdentifier("\n"), global],
+        `${filePath}/types.d.ts`
+    );
+};
